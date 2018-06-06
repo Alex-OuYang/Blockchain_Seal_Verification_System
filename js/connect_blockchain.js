30c13d857dd6d2015f4ff1750ead4db83ad28172
ALEX-NB\alex1 <alex1101137204@gmail.com>
2018-06-06 22:36:21 +0800

    修改比對門檻 95 -> 90 修改連結 ip

--- js/connect_blockchain.js
+++ js/connect_blockchain.js
@@ -1,12 +1,13 @@
 //連接區塊鏈
-//var connectIP = "140.124.93.79";
-var connectIP = "localhost";
+var connectIP = "140.124.93.79";
+//var connectIP = "localhost";
 var Web3 = require('web3');
 var web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider("http://" + connectIP + ":8545"));
 var isConnected = web3.isConnected();
 //console.log(isConnected);
 if (!isConnected) {
-    alert("請先連接區塊鏈！！");
+    //alert("請先連接區塊鏈！！");
+    alert(connectIP + " 未連接區塊鏈！！");
 }
 //連接區塊鏈
